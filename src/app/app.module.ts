6dfdd1809144c20e5b819a6714fb95b938bfe67e
Thomas Frey <joe@mitlinxlernen.ch>
2020-06-05 17:21:25 +0200

    erste Beispiel Componente erstellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesOverviewComponent } from './courses-overview/courses-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesOverviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses-overview/courses-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-overview',
+  templateUrl: './courses-overview.component.html',
+  styleUrls: ['./courses-overview.component.scss']
+})
+export class CoursesOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
